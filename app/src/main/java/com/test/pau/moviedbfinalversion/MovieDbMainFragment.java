028265867de2934a809234df8927daed3f919beb
pauProva <paudam1990@gmail.com>
2015-11-10 12:36:39 +0100

    Initial commit

---
+++ app/src/main/java/com/test/pau/moviedbfinalversion/MovieDbMainFragment.java
@@ -0,0 +1,22 @@
+package com.test.pau.moviedbfinalversion;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MovieDbMainFragment extends Fragment {
+
+    public MovieDbMainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_movie_db_main, container, false);
+    }
+}
